da17b93e77adc95d1a848b5449d9b0d9905de311
MeirambekPY <zhalgasbekmeirambek06@gmail.com>
2026-02-16 12:02:42 +0500

    Create product-card.component.ts

---
+++ Lab4/task2/online-store/src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent {
+  @Input() product!: Product;
+
+  getStars(rating: number): number[] {
+    return Array(Math.round(rating)).fill(0);
+  }
+}
